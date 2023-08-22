237e3b7892d10e77ad9170b9d27d9579ded082ad
aych <aychdev1@gmail.com>
2023-08-21 21:06:32 -0400

    Added unit tests to Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,24 +1,21 @@
+// src/Header.test.js
 import React from 'react';
+import { render } from '@testing-library/react';
+import Header from './Header';
 
-function Header() {
-  return (
-    <header>
-      <h1>Welcome to Little Lemon</h1>
-      <nav>
-        <ul>
-          <li>
-            <a href='#menu'>Menu</a>
-          </li>
-          <li>
-            <a href='#about'>About Us</a>
-          </li>
-          <li>
-            <a href='#contact'>Contact</a>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
-}
+test('renders header with correct title', () => {
+  const { getByText } = render(<Header />);
+  const headerElement = getByText(/Welcome to Little Lemon/i);
+  expect(headerElement).toBeInTheDocument();
+});
 
-export default Header;
+test('renders navigation links', () => {
+  const { getByText } = render(<Header />);
+  const menuLink = getByText(/Menu/i);
+  const aboutLink = getByText(/About Us/i);
+  const contactLink = getByText(/Contact/i);
+
+  expect(menuLink).toBeInTheDocument();
+  expect(aboutLink).toBeInTheDocument();
+  expect(contactLink).toBeInTheDocument();
+});
